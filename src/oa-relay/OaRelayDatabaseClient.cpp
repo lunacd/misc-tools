ffa14e7de13cf2d2a4ed00ef2d2a2938c23899bc
Haowen Liu <lhw@lunacd.com>
2025-03-02 19:58:02 -0500

    Remove unused oaRelay

--- src/oa-relay/OaRelayDatabaseClient.cpp
+++
@@ -1,1 +0,0 @@
-#include <OaRelayDatabaseClient.hpp>
--- src/oa-relay/auth/OaRelayAuthHandler.cpp
+++
@@ -1,9 +0,0 @@
-#include <auth/OaRelayAuthHandler.hpp>
-
-namespace Lunacd::OaRelay {
-std::shared_ptr<oatpp::web::server::handler::AuthorizationObject>
-AuthHandler::authorize(const oatpp::String &token) {
-  const auto payload = m_jwt->readAndVerifyToken(token);
-  return payload;
-}
-} // namespace Lunacd::OaRelay
