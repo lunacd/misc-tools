e3bc135917929478089aa5c61da22d06dba232da
Haowen Liu <lhw@lunacd.com>
2025-03-16 16:17:52 -0400

    Remove unused frontend

--- frontend/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: "standalone",
-  basePath: "/ui",
-};
-
-export default nextConfig;
